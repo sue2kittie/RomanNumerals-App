856bcbcdb624975b96af5c462927cc20f45a6fed
Sura K <skarnawi@gmail.com>
2019-07-14 23:13:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# README - Roman Numerals#
+
+### This is Numbers to Roman Numerals app ###
+
+
+### How do I build the app? ###
+* `npm install`
+*  `npm install -g browser-sync`
+
+
+## running
+* `gulp` on localhost:3002
+*  gulp mocha, gulp watch-mocha
+
+
+### Contribution guidelines ###
+
+* Writing tests
+* Code review
+* Sura K is on her journey to learn 
+
+
+
+
+
+
---
+++ app/javascript/app.js
@@ -0,0 +1,31 @@
+'user strict';
+
+let number, numberLength, output, numberArray,result ;
+
+const romanNumerals =[["", "I", "II", "III", "IV", "V", "VI", "VII","VIII","IX" ],
+                      ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX","LXXX","XC"],
+                      ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC","DCCC","CM"],
+                      ["", "M", "MM", "MMM", ""  , "",  ""  , ""   , ""    ,""]];
+
+function convert() {
+
+  number = document.getElementById('number').value;
+
+  if (number < 3999 ) {
+      numberArray = Array.from(number.toString()).reverse();
+      numberLength = numberArray.length;
+      result  ='';
+      
+      numberArray.forEach((item, index) => {
+      result = romanNumerals[index][item] + result;
+  });
+    //console.log('result >>>>', result);
+    document.getElementById('result').innerHTML = result;
+
+  } else {
+    alert('Please enter a number from 0-3999');
+  }
+
+
+}
+  
